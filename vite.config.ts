53439811ed15f6b61c38cb1c77fbb09701664330
Choco <cindymeow905@gmail.com>
2025-04-26 14:50:06 +0800

    removed vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     server: {
         host: "0.0.0.0"
     },
-    plugins: [react()],
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        sourcemap: true
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src')
-        }
-    }
+    plugins: [react()]
 });
